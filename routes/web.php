76ebdecdf647b89b28ac1ede3ce6513bff104900
daiyahome <honbu@daiyahome.jp>
2025-01-30 15:17:35 +0900

    アップロード

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\PostController;
+// ルーティングを設定するコントローラーの宣言をする
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
+use App\Http\Controllers\RequestController;
+use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\SignInController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +24,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hello', [HelloController::class, 'index']);
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index']);
 
-Route::get('posts/{id}', [PostController::class, 'show']);
+Route::get('/products/create', [ProductController::class, 'create']);
+
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::get('/vendors/create', [VendorController::class, 'create']);
+
+Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
+
+Route::get('/vendors/{id}', [VendorController::class, 'show']);
+
+Route::get('/requests/create', [RequestController::class, 'create']);
+
+Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
+
+Route::get('/responses', [ResponseController::class, 'index']);
+
+Route::get('/sign-in', [SignInController::class, 'create']);
+
+Route::post('/sign-in', [SignInController::class, 'store'])->name('sign-in.store');
